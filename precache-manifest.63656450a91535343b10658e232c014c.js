c1cb19a2279468749bbd8bce56fe9c6f8abdbe47
Carol Alonso <c.alonsolp@gmail.com>
2020-03-03 17:33:00 +0100

    Updates

---
+++ precache-manifest.63656450a91535343b10658e232c014c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "816011c06eebebdb259ffbb5e04283f6",
+    "url": "/react-trello/index.html"
+  },
+  {
+    "revision": "df6c60c07829b9602fa8",
+    "url": "/react-trello/static/css/2.64159879.chunk.css"
+  },
+  {
+    "revision": "159b891c0251d47d6353",
+    "url": "/react-trello/static/css/main.58d21e84.chunk.css"
+  },
+  {
+    "revision": "df6c60c07829b9602fa8",
+    "url": "/react-trello/static/js/2.ea7945e6.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-trello/static/js/2.ea7945e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "159b891c0251d47d6353",
+    "url": "/react-trello/static/js/main.6c81911e.chunk.js"
+  },
+  {
+    "revision": "c0861a95f6220e985b66",
+    "url": "/react-trello/static/js/runtime-main.d280c78b.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-trello/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-trello/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-trello/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-trello/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-trello/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
