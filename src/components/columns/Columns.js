a2dda70f11314bc2176a51c2473d19a551135759
Carol Alonso <c.alonsolp@gmail.com>
2020-03-03 17:48:19 +0100

    redirect button

--- src/components/columns/Columns.js
+++ src/components/columns/Columns.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import { getColumns } from '../../services/TrelloService';
 import Column from './Column';
 import './Columns.css';
+import { Redirect } from 'react-router-dom';
 
 export default class Columns extends Component {
 	state = {
 		columns: [],
-		error: undefined
+		error: undefined,
+		redirect: false
 	};
 
 	componentDidMount() {
@@ -21,10 +23,18 @@ export default class Columns extends Component {
 		);
 	}
 
+	redirect() {
+		this.setState({ redirect: true })
+	}
+
 	render() {
+		if (this.state.redirect) {
+			return (<Redirect to="/pepe" />)
+		}
 		const columns = this.state.columns.map(column => <Column key={column.id} {...column} />)
 		return (
 			<div className='Columns'>
+				<button onClick={() => this.redirect()}>Redirect</button>
 				{columns}
 			</div>
 		)
